dcb3b19ad23717e33ca02ddde8a1598a774b617f
Raunak-lab <raunakkumarthakur12@gmail.com>
2022-11-20 10:40:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+   <>
+   <Navbar/>
+   {/* <Home/>
+   <Main/>
+   <Footer/> */}
+   </>
+  )
+}
+
+export default App
